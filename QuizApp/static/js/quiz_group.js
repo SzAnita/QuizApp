2a1151aa12df189e4b4a5e5b5d3da62c57e3c1f3
SzAnita <97391261+SzAnita@users.noreply.github.com>
2024-05-27 15:50:30 +0300

    update quizgroup

--- QuizApp/static/js/quiz_group.js
+++ QuizApp/static/js/quiz_group.js
@@ -1,13 +1,24 @@
 $(document).on("submit", ".add_to_group", function (event) {
     event.preventDefault();
+    let groups = [];
+    $('input[name=group]:checked').each(function() {
+        groups.push(this.value);
+    });
     $.ajax({
         url: 'quiz_group',
         method: 'PUT',
-        headers: {'X-CSRFToken': $(this).find("input[name=csrfmiddlewaretoken]").val()},
+        headers: {
+            'X-CSRFToken': $(this).find("input[name=csrfmiddlewaretoken]").val(),
+            'Content-Type': 'application/JSON'
+        },
+        contentType: 'application/JSON',
         data: {
-            'quiz_id': $(this).find('input[name=quiz_id]').val(),
-            'group': $('input[name=group]:checked').val(),
-            'type': 'add'
+            "quiz_id": $(this).find('input[name=quiz_id]').val(),
+            "groups": groups,
+            "type": "add"
+        },
+        error: function(xhr) {
+            alert(JSON.parse(xhr.responseText).msg);
         }
     })
 })
@@ -35,7 +46,6 @@ function new_group(quiz_id) {
 $(document).on("click", ".btn-create", function (event) {
     event.preventDefault();
     let this_ = $(this);
-    //let quiz_id = $(this).parent().parent().find("input[name=quiz_id]").val();
     let group_name = $(this).parent().parent().find("input[name=new_group_name]").val();
     $.ajax({
         url: 'quiz_group',
@@ -46,10 +56,14 @@ $(document).on("click", ".btn-create", function (event) {
             'group': group_name
         },
         success: function (data) {
-            let new_checkbox = $("<input type='checkbox' id='group'+data.group_id name='group' value=data.group_name >")
-            let label = $("<label for=data.group_name>data.group_name</label>")
-            let group_nr = this_.closest("div.group-list").find("input[name=group]").length;
-            this_.closest("div.group-list").find("label").eq(group_nr-1).after(new_checkbox, label, "<br>");
+            alert(data.group_id);
+            alert(data.group_name);
+            let new_checkbox = $("<input type='checkbox' id='group"+data.group_id+"' name='group' value='"+data.group_name+"'>");
+            let label = $("<label for='"+data.group_name+">"+data.group_name+"</label>");
+            new_checkbox.after(label, "<br>");
+            alert(new_checkbox.html());
+            let group_nr = this_.closest("div.new-group").parent().find("div.group-list").find("input[name=group]").length;
+            this_.closest("div.new-group").parent().find("div.group-list").find("label").eq(group_nr-1).after("<br>", new_checkbox);
         }
     })
 })
@@ -57,8 +71,7 @@ $(document).on("click", ".btn-create", function (event) {
 $(document).on("submit", ".edit_group", function (event) {
     event.preventDefault();
     let new_name_val = $(this).find("input[name=g_name]").val();
-    let old_val = $(this).closest("div.group-header").find("h3").first();
-    alert(old_val.text());
+    let old_val = $(this).closest("tr.group_header").find("h3").first();
     $.ajax({
         url: 'quiz_group',
         method: 'PUT',
@@ -71,4 +84,20 @@ $(document).on("submit", ".edit_group", function (event) {
             old_val.text(new_name_val);
         }
     })
+})
+
+$(document).on("click", ".delete-quizgroup", function(event) {
+    event.preventDefault();
+    let this_ = $(this);
+    $.ajax({
+        url: 'quiz_group',
+        method: 'DELETE',
+        headers: {'X-CSRFToken': getCSRFToken()},
+        data: {'group_id': $(this).attr('id')},
+        success: function (data) {
+            this_.closest("table").remove();
+            this_.closest("table").remove();
+        }
+    })
+
 })
--- QuizApp/views/quiz/QuizGroupView.py
+++ QuizApp/views/quiz/QuizGroupView.py
@@ -1,50 +1,83 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse, HttpResponseRedirect, QueryDict, HttpResponse
+from django.http import JsonResponse, QueryDict, HttpResponse
 from django.template import loader
 from django.views import View
+from django.views.generic.base import TemplateResponseMixin, TemplateView
 
 from QuizApp.models import QuizGroup, Quiz
 
 
-class QuizGroupView(View, LoginRequiredMixin):
+class QuizGroupView(LoginRequiredMixin, TemplateView, View):
+    template_name = 'quiz/quiz-group.html'
     def get(self, request):
         context = {
             'groups': QuizGroup.objects.filter(owner_id=request.user),
             'auth': 'yes',
-            'my_groups': 'yes'
+            'my_groups': 'yes',
         }
-        template = loader.get_template('quiz/quiz-group.html')
-        return HttpResponse(template.render(context, request))
+        return self.render_to_response(context)
 
     def post(self, request):
-        if request.user.is_authenticated:
+        if not QuizGroup.objects.filter(owner_id=request.user, name=request.POST['group']).exists():
             QuizGroup(owner_id=request.user, name=request.POST['group']).save()
-            group = QuizGroup(owner_id=request.user, name=request.POST['group'])
+            group = QuizGroup.objects.get(owner_id=request.user, name=request.POST['group'])
             return JsonResponse({'group_id': group.id, 'group_name': group.name}, status=201)
         else:
-            return HttpResponseRedirect('QuizApp/login')
+            return JsonResponse({'msg': "You already have a quiz group with this name."}, status=406)
 
     def put(self, request):
         put = QueryDict(request.body)
-        if (put['type'] == 'add' or put['type'] == 'remove') and  Quiz.objects.filter(id=put['quiz_id']).exists():
-            quiz = Quiz.objects.get(id=put['quiz_id'])
-            if put['type'] == 'add':
-                quiz_group = QuizGroup.objects.get(owner_id=request.user, name=put['group'])
-                if not QuizGroup.objects.filter(owner_id=request.user, name=put['group'], quizzes__id=quiz.id).exists():
-                    quiz_group.quizzes.add(quiz)
-                    quiz_group.save()
-
+        if hasattr(request, 'PUT'):
+            put = request.PUT
+        if put['type'] == 'add' or put['type'] == 'remove':
+            if Quiz.objects.filter(id=put['quiz_id']).exists():
+                quiz = Quiz.objects.get(id=put['quiz_id'])
+                if put['type'] == 'add':
+                    status = 400
+                    data = {'msg': 'This quiz is already in the quiz groups'}
+                    for g in put.getlist('groups[]'):
+                        if QuizGroup.objects.filter(owner_id=request.user, name=g).exists():
+                            quiz_group = QuizGroup.objects.get(owner_id=request.user, name=g)
+                            if not QuizGroup.objects.filter(owner_id=request.user, name=g, quizzes__id=quiz.id).exists():
+                                quiz_group.quizzes.add(quiz)
+                                quiz_group.save()
+                                status = 200
+                        else:
+                            data['msg'] = "This quiz group doesn't exist"
+                            status = 404
+                    return JsonResponse(data, status=status)
+                elif put['type'] == 'remove':
+                    if QuizGroup.objects.filter(owner_id=request.user, id=put['group_id']).exists():
+                        quiz_group = QuizGroup.objects.get(owner_id=request.user, id=put['group_id'])
+                        quiz_group.quizzes.remove(quiz)
+                        return JsonResponse({}, status=200)
+                    else:
+                        return JsonResponse({"msg": "This quiz group doesn't exist"}, status=404)
+            else:
+                return JsonResponse({"msg": "This quiz doesn't exist"}, status=404)
+        elif put['type'] == 'name':
+            if QuizGroup.objects.filter(owner_id=request.user, name=put['old_name']).exists():
+                group = QuizGroup.objects.get(owner_id=request.user, name=put['old_name'])
+                if not QuizGroup.objects.filter(owner_id=request.user, name=put['new_name']).exists():
+                    group.name = put['new_name']
+                    group.save()
                     return JsonResponse({}, status=200)
                 else:
-                    return JsonResponse({}, status=400)
-            elif put['type'] == 'remove':
-                quiz_group = QuizGroup.objects.get(owner_id=request.user, id=put['group_id'])
-                quiz_group.quizzes.remove(quiz)
+                    return JsonResponse({"msg": "You already have a quiz group with this name."}, status=409)
+            else:
+                return JsonResponse({"msg": "This quiz group doesn't exist"}, status=404)
+
+    def delete(self, request):
+        delete = QueryDict(request.body)
+        if hasattr(request, 'DELETE'):
+            delete = request.DELETE
+        if QuizGroup.objects.filter(id=int(delete['group_id'])).exists():
+            group = QuizGroup.objects.get(id=int(delete['group_id']))
+            if group.owner_id == request.user:
+                group.delete()
                 return JsonResponse({}, status=200)
-        elif put['type'] == 'name':
-            group = QuizGroup.objects.get(owner_id=request.user, name=put['old_name'])
-            group.name = put['new_name']
-            group.save()
-            return JsonResponse({}, status=200)
+            else:
+                return JsonResponse({}, status=401)
         else:
-            return JsonResponse({}, status=404)
+            return JsonResponse({'msg': "This group doesn't exist"}, status=404)
+
