41d0dfbb626695523325b356b0ebc2b7e33ba3e7
SzAnita <97391261+SzAnita@users.noreply.github.com>
2024-05-27 15:59:04 +0300

    update sign up and user page

--- QuizApp/views/user/UserView.py
+++ QuizApp/views/user/UserView.py
@@ -1,81 +1,61 @@
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import check_password
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
-from django.http import HttpResponse, HttpResponseRedirect, QueryDict, JsonResponse
-from django.template import loader
-from django.utils.decorators import method_decorator
+from django.http import HttpResponseRedirect, QueryDict, JsonResponse
+from django.urls import reverse
 from django.views import View
+from django.views.generic import CreateView
 
 from QuizApp.forms import Signup
 from QuizApp.models import User
 
 
-class UserView(View):
+class SignUpView(CreateView):
+    model = User
+    form_class = Signup
+    template_name = 'authentication/signup.html'
 
-    # display signup page
-    def get(self, request):
-        context = {'form': Signup}
-        return HttpResponse(loader.get_template('signup.html').render(context, request))
+    def form_valid(self, form):
+        login(self.request, form.save())
+        return HttpResponseRedirect(reverse('home'))
 
-    def post(self, request):
-        form = Signup(request.POST)
-        context = {'form': form}
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            pwd = form.cleaned_data.get('pwd')
-            name = None
-            username = email
-            user = User(email=email, password=pwd)
-            if len(form.cleaned_data['name']) > 0:
-                name = form.cleaned_data['name']
-                user.name = name
-            if len(form.cleaned_data['username']) > 0:
-                username = form.cleaned_data['username']
-                user.username = username
-            if not User.objects.filter(email=email).exists():
-                try:
-                    validate_password(pwd, user)
-                    user.set_password(pwd)
-                    user.save()
-                    login(request, user)
-                    request.session['email'] = email
-                    request.session.modified = True
-                    return HttpResponseRedirect('../home')
-                except ValidationError as e:
-                    form.add_error('pwd', e)
-                    context['form'] = form
-        return HttpResponse(loader.get_template('signup.html').render(context, request))
+
+class UserView(LoginRequiredMixin, View):
+    login_url = 'QuizApp/login'
 
     # update user info
     def put(self, request):
-        if request.user.is_authenticated:
-            put = QueryDict(request.body)
-            user = request.user
-            errors = []
-            if 'username' in put:
-                if not User.objects.filter(username=put['username']).exists():
-                    user.username = put['username']
-                else:
-                    errors.append('This username already exists')
-            if 'email' in put:
-                if check_password(put['pwd'], user.password):
-                    if not User.objects.filter(email=put['email']).exists():
-                        user.email = put['email']
-                    else:
-                        errors.append('This email already has an account')
-                else:
-                    errors.append("The given password doesn't match with your password")
-            if 'old_pwd' in put:
-                if check_password(put['old_pwd'], user.password):
-                    user.set_password(put['pwd'])
-                else:
-                    errors.append('Make sure that your old password is correct')
-            user.save()
-            if len(errors) > 0:
-                return JsonResponse({'errors': errors}, status=200)
+        put = QueryDict(request.body)
+        if hasattr(request, 'PUT'):
+            put = request.PUT
+        user = request.user
+        errors = []
+        if 'username' in put:
+            if not User.objects.filter(username=put['username']).exists():
+                user.username = put['username']
             else:
-                return JsonResponse({}, status=200)
+                errors.append('This username already exists')
+        elif 'email' in put:
+            if check_password(put['pwd'], user.password):
+                if not User.objects.filter(email=put['email']).exists():
+                    user.email = put['email']
+                else:
+                    errors.append('This email already has an account')
+            else:
+                errors.append("The given password doesn't match with your password")
+        elif 'old_pwd' in put:
+            if check_password(put['old_pwd'], user.password):
+                try:
+                    validate_password(put['pwd'])
+                    user.set_password(put['pwd'])
+                except ValidationError:
+                    errors.append('Make sure to provide a valid password')
+            else:
+                errors.append('Make sure that your old password is correct')
+        user.save()
+        if len(errors) > 0:
+            return JsonResponse({'errors': errors}, status=400)
         else:
-            return HttpResponseRedirect('QuizApp/login')
+            return JsonResponse({}, status=200)
